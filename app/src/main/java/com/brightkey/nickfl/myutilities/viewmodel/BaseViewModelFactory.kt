e80b85af418276e252850c5a9267212a37b93be7
nickfl <nfloussov@live.com>
2020-08-24 15:37:14 -0400

    enbridge bill

--- app/src/main/java/com/brightkey/nickfl/myutilities/viewmodel/BaseViewModelFactory.kt
+++ app/src/main/java/com/brightkey/nickfl/myutilities/viewmodel/BaseViewModelFactory.kt
@@ -3,7 +3,7 @@ package com.brightkey.nickfl.myutilities.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
-class BaseViewModelFactory(private val arg: String) :ViewModelProvider.Factory {
+class BaseViewModelFactory(private val arg: String): ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         return modelClass.getConstructor(String::class.java).newInstance(arg)
     }
